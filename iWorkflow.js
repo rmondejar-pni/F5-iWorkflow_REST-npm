667f70df6fc101da73eca325290f9022ea0d9e59
Nathan Pearce <nathan.pearce@gmail.com>
2016-09-02 12:17:31 -0700

    Example inout file for Service deployment
    Provided an example JSON payload for deploying a basic f5.http service

--- iWorkflow.js
+++ iWorkflow.js
@@ -1,15 +1,11 @@
 //iWorkflow.js
 const fs = require('fs');
 var request = require('request');
-var config = require('./config.js')
+var config = require('./config.js') //Collect environment options
 
-// Set default options
-this.host   = (typeof config.host   === 'string')  ? config.host   : '127.0.0.1';
-this.strict = (typeof config.strict === 'boolean') ? config.strict : true;
-this.debug  = (typeof config.debug  === 'boolean') ? config.debug  : false;
 // Allow self-signed cert if strict not set
 // https://github.com/joyent/node/pull/4023
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = (this.strict) ? "1" : "0";
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = (config.strict) ? "1" : "0";
 
 var myArgs = process.argv.slice(2);
 
@@ -22,42 +18,42 @@ if (config.debug) {
 switch (myArgs[0]) {
   case 'init':
     if (myArgs[1] === 'help' || myArgs.length < 3)  {
-      console.log('Usage: ./iWorkflow.js init [host] [username] [password]')
+      console.log('Usage: ./iWorkflow.js init [host] [username] [password]\n');
+      console.log('\tThe \'init\' command retrieves an iWorkflow Auth token and creates the config.js file used by other iWorkflow.js commands');
     }
     else {
-      console.log('Firing inti topedos');
       exec_init(myArgs[1],myArgs[2],myArgs[3]);
     }
     break;
 
   case 'list':
     if (myArgs[0] === 'help' || myArgs.length < 2)  {
-      console.log('Usage: ./iWorkflow.js list [tenant|template]')
+      console.log('Usage: ./iWorkflow.js list [tenant|template]\n');
+      console.log('\tThe \'list\' command retreives Tenant resource names from iWorkflow.');
     }
     else {
-      console.log('Firing list torpedos! Not yet implemented.');
-//      exec_list(myArgs[2]);
+      exec_list(myArgs[1]);
     }
     break;
 
-    case 'deploy':
-      if (myArgs[0] === 'help' || myArgs.length < 3)  {
-        console.log('Usage: ./iWorkflow.js deploy [file.json]')
-      }
-      else {
-        console.log('This is deploy! Not yet implemented.');
-      }
-      break;
+  case 'deploy':
+    if (myArgs[0] === 'help' || myArgs.length < 3)  {
+      console.log('Usage: ./iWorkflow.js deploy [input.json]\n');
+      console.log('\t The \'deploy\' command is used to deploy an L4 - L7 Service using an iWorflow L4 - L7 Services Template. This command requires a JSON formatted input file.');
+    }
+    else {
+      console.log('This is deploy! Not yet implemented.');
+    }
+    break;
 
-      case 'delete':
-        if (myArgs[0] === 'help' || myArgs.length < 3)  {
-          console.log('Usage: ./iWorkflow.js delete [file.json]')
-        }
-        else {
-          console.log('This is delete! Not yet implemented.');
-        }
-        break;
-
+  case 'delete':
+    if (myArgs[0] === 'help' || myArgs.length < 3)  {
+      console.log('Usage: ./iWorkflow.js delete [file.json]');
+    }
+    else {
+      console.log('This is delete! Not yet implemented.');
+    }
+    break;
 
   default:
     console.log('Usage: iWorkflow.js <command> [options]\n');
@@ -82,17 +78,21 @@ function exec_init (host, username, password) {
      { username: username,
        password: password,
        loginProvidername: 'tmos' },
-    json: true };
+    json: true
+  };
 
   request(options, function (error, response, body) {
-    if (error) throw new Error(error);
+//    if (error) throw new Error(error);
+    if (error && response.statusCode == 401) {
+       console.log('Invalid credentials\nError:' +error);
+     };
 
     var token = body.token.token;
-    if (this.debug) { console.log('token:' +token)};
+    if (config.debug) { console.log('token:' +token)};
 
     //write to config.js
     var conf_data = 'module.exports = {\n\thost: \''+host+'\',\n\ttoken: \''+token+'\',\n\tstrict: '+config.strict+',\n\tdebug: '+config.debug+'\n};';
-    if (this.debug) { console.log(conf_data)};
+    if (config.debug) { console.log(conf_data)};
 
     fs.writeFile('config.js', conf_data, (err) => {
       if (err) throw err;
@@ -102,5 +102,46 @@ function exec_init (host, username, password) {
 };
 
 function exec_list (type) {
-  console.log('In exec_list with args: nothing to do');
+  if (config.debug) { console.log('In exec_list with Args: ' +type)};
+
+  //build options.
+  if (type === 'tenant')  {
+    var options = { method: 'GET',
+//      url: 'https://'+config.host+'/mgmt/shared/authz/roles',
+      url: 'https://'+config.host+'/mgmt/shared/authz/roles?$select=displayName&$filter=displayName%20eq%20\'*Cloud%20Tenant*\'',
+//      ?$select=displayName&$filter=displayName%20eq%20\'*Cloud%20Tenant*\'
+      headers:
+       { 'cache-control': 'no-cache',
+         'x-f5-auth-token': config.token }
+    };
+
+    request(options, function (error, response, body) {
+  //    if (error) throw new Error(error);
+      console.log('response.statusCode: ' +response.statusCode);
+      if (response.statusCode == '401') {
+         console.log('Auth Token expired. Re-initialize with \'iWorkflow.js init\'');
+       }
+       else if (response.statusCode == '200'){
+         console.log('data: ' +JSON.parse(body));
+//         console.log('stuff: ' +parsedJSON['displayName']);
+
+       }
+       else if (error) throw new Error(error);
+    });
+  }
+  else if (type === 'template') {
+    var options = {
+      method: 'GET',
+      url: 'https://'+config.host+'/mgmt/cm/cloud/tenant/templates/iapp/',
+      headers:
+       { 'cache-control': 'no-cache',
+         'x-f5-auth-token': config.token }
+    };
+
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+      console.log(body.name);
+    });
+
+  }
 };
